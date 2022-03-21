9795c140bcf1bd73107bbd5074767d243c2b5083
Oluwasheun Lawani <oluwaseun.lawani@iisysgroup.com>
2022-03-21 17:00:58 +0100

    Scanner ongoing

--- lox/com/craftinginterpreters/lox/Lox.java
+++ lox/com/craftinginterpreters/lox/Lox.java
@@ -1,4 +1,3 @@
-//> Scanning lox-class
 package com.craftinginterpreters.lox;
 
 import java.io.BufferedReader;
@@ -10,102 +9,53 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-//> Evaluating Expressions interpreter-instance
-  private static final Interpreter interpreter = new Interpreter();
-//< Evaluating Expressions interpreter-instance
-//> had-error
   static boolean hadError = false;
-//< had-error
-//> Evaluating Expressions had-runtime-error-field
-  static boolean hadRuntimeError = false;
 
-//< Evaluating Expressions had-runtime-error-field
+
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
-      System.exit(64); // [64]
+      System.exit(64); 
     } else if (args.length == 1) {
       runFile(args[0]);
     } else {
       runPrompt();
     }
   }
-//> run-file
-  private static void runFile(String path) throws IOException {
+
+
+private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
-//> exit-code
 
-    // Indicate an error in the exit code.
-    if (hadError) System.exit(65);
-//< exit-code
-//> Evaluating Expressions check-runtime-error
-    if (hadRuntimeError) System.exit(70);
-//< Evaluating Expressions check-runtime-error
+        if (hadError) System.exit(65);
+
   }
-//< run-file
-//> prompt
+
   private static void runPrompt() throws IOException {
     InputStreamReader input = new InputStreamReader(System.in);
     BufferedReader reader = new BufferedReader(input);
 
-    for (;;) { // [repl]
+    for (;;) { 
       System.out.print("> ");
       String line = reader.readLine();
       if (line == null) break;
       run(line);
-//> reset-had-error
       hadError = false;
-//< reset-had-error
+
     }
   }
-//< prompt
-//> run
+
   private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
-/* Scanning run < Parsing Expressions print-ast
 
     // For now, just print the tokens.
     for (Token token : tokens) {
       System.out.println(token);
     }
-*/
-//> Parsing Expressions print-ast
-    Parser parser = new Parser(tokens);
-/* Parsing Expressions print-ast < Statements and State parse-statements
-    Expr expression = parser.parse();
-*/
-//> Statements and State parse-statements
-    List<Stmt> statements = parser.parse();
-//< Statements and State parse-statements
-
-    // Stop if there was a syntax error.
-    if (hadError) return;
-
-//< Parsing Expressions print-ast
-//> Resolving and Binding create-resolver
-    Resolver resolver = new Resolver(interpreter);
-    resolver.resolve(statements);
-//> resolution-error
-
-    // Stop if there was a resolution error.
-    if (hadError) return;
-//< resolution-error
-
-//< Resolving and Binding create-resolver
-/* Parsing Expressions print-ast < Evaluating Expressions interpreter-interpret
-    System.out.println(new AstPrinter().print(expression));
-*/
-/* Evaluating Expressions interpreter-interpret < Statements and State interpret-statements
-    interpreter.interpret(expression);
-*/
-//> Statements and State interpret-statements
-    interpreter.interpret(statements);
-//< Statements and State interpret-statements
   }
-//< run
-//> lox-error
+
   static void error(int line, String message) {
     report(line, "", message);
   }
@@ -116,21 +66,5 @@ public class Lox {
         "[line " + line + "] Error" + where + ": " + message);
     hadError = true;
   }
-//< lox-error
-//> Parsing Expressions token-error
-  static void error(Token token, String message) {
-    if (token.type == TokenType.EOF) {
-      report(token.line, " at end", message);
-    } else {
-      report(token.line, " at '" + token.lexeme + "'", message);
-    }
-  }
-//< Parsing Expressions token-error
-//> Evaluating Expressions runtime-error-method
-  static void runtimeError(RuntimeError error) {
-    System.err.println(error.getMessage() +
-        "\n[line " + error.token.line + "]");
-    hadRuntimeError = true;
-  }
-//< Evaluating Expressions runtime-error-method
+
 }
